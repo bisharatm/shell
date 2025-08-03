7c426078b3582ff46f5213dcf2bc12f430e490d0
Bisharat Memon <2830568+bisharatm@users.noreply.github.com>
2025-08-03 17:05:36 -0400

    modified assignment.sh script to force remove all files containing IP addresses from raw and processed data directories (7)

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -47,6 +47,8 @@ cp data/raw/user_log_*.log data/processed/user_logs
 cp data/raw/event_log_*.log data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm -f data/raw/*ipaddr*
+rm -f data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
